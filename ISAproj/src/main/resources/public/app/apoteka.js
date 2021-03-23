b8919d3a24223423f56065673d65a795ce21543a
Nikola <Win10@DESKTOP-K5N99LH>
2021-03-23 23:41:35 +0100

    Added table display

--- ISAproj/src/main/resources/public/app/apoteka.js
+++ ISAproj/src/main/resources/public/app/apoteka.js
@@ -9,9 +9,27 @@ Vue.component("Apoteka", {
   	},
     template: `
     	<div>
-    	<p>
-    		{{apoteke}}
-		</p>
+    	<table>
+    		<tr>
+    			<th>id</th>
+    			<th>naziv</th> 
+    			<th>adresa</th>
+    			<th>prosecna ocena</th>
+    			<th>opis</th>
+    			<th>dermatolozi</th>
+    			<th>farmaceuti</th>
+    		</tr>
+    		<tr>
+    			<td>{{apoteke.id}}</td>
+    			<td>{{apoteke.naziv}}</td>
+    			<td>{{apoteke.adresa}}</td>
+    			<td>{{apoteke.prosecnaOcena}}</td>
+    			<td>{{apoteke.opis}}</td>
+    			<td>{{apoteke.dermatolozi}}</td>
+    			<td>{{apoteke.farmaceuti}}</td>
+    			
+    		</tr>
+    	</table>
    	  	<div>
         	<button v-on:click="redirectToHome">Home Page</button>
       	</div>
--- ISAproj/src/main/resources/public/app/apoteke.js
+++ ISAproj/src/main/resources/public/app/apoteke.js
@@ -9,10 +9,27 @@ Vue.component("Apoteke", {
   	},
     template: `
     	<div>
-    	<ul>
-    		<li v-for="apoteka in apoteke" :key="apoteka.id">
-    		{{apoteka}}</li>
-    	</ul>
+    	<table>
+    		<tr>
+    			<th>id</th>
+    			<th>naziv</th> 
+    			<th>adresa</th>
+    			<th>prosecna ocena</th>
+    			<th>opis</th>
+    			<th>dermatolozi</th>
+    			<th>farmaceuti</th>
+    		</tr>
+    		<tr v-for="apoteka in apoteke" :key="apoteka.id">
+    			<td>{{apoteka.id}}</td>
+    			<td>{{apoteka.naziv}}</td>
+    			<td>{{apoteka.adresa}}</td>
+    			<td>{{apoteka.prosecnaOcena}}</td>
+    			<td>{{apoteka.opis}}</td>
+    			<td>{{apoteka.dermatolozi}}</td>
+    			<td>{{apoteka.farmaceuti}}</td>
+    			
+    		</tr>
+    	</table>
    	  	<div>
         	<button v-on:click="redirectToHome">Home Page</button>
       	</div>
