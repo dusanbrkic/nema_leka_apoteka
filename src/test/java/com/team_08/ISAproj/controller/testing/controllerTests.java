084875d66a3e784326623ae3dd10760ff07d2311
Hajduk Dušan <62693918+HajdukDusan@users.noreply.github.com>
2021-06-15 00:40:33 +0200

    Travis test

--- src/test/java/com/team_08/ISAproj/controller/testing/controllerTests.java
+++ src/test/java/com/team_08/ISAproj/controller/testing/controllerTests.java
@@ -17,6 +17,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -27,6 +28,9 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 
+import com.team_08.ISAproj.app.ISAprojApplication;
+
+@WebMvcTest(ISAprojApplication.class)
 @SpringBootApplication(scanBasePackages={"com.team_08.ISAproj"})
 @RunWith(SpringRunner.class)
 @SpringBootTest
