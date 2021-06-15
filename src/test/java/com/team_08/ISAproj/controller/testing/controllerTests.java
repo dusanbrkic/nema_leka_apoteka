d356e868f2c5499f0eed0827c85e72c10323b422
Hajduk Dušan <62693918+HajdukDusan@users.noreply.github.com>
2021-06-15 11:32:18 +0200

    fix 6

--- src/test/java/com/team_08/ISAproj/controller/testing/controllerTests.java
+++ src/test/java/com/team_08/ISAproj/controller/testing/controllerTests.java
@@ -23,6 +23,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -32,7 +33,7 @@ import org.springframework.web.context.WebApplicationContext;
 import com.team_08.ISAproj.app.ISAprojApplication;
 import com.team_08.ISAproj.controller.LekController;
 
-@AutoConfigureMockMvc
+@ContextConfiguration(classes = ISAprojApplication.class)
 @SpringBootApplication(scanBasePackages={"com.team_08.ISAproj"})
 @RunWith(SpringRunner.class)
 @SpringBootTest
