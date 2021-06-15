29c750099873b16a1d2526aa9979025322a995ee
Dusan Brkic <dusanbrk@gmail.com>
2021-06-15 21:15:43 +0200

    testovi

--- src/test/java/com/team_08/ISAproj/service/testing/serviceTests.java
+++ src/test/java/com/team_08/ISAproj/service/testing/serviceTests.java
@@ -1,14 +1,12 @@
 package com.team_08.ISAproj.service.testing;
 
 
-import com.team_08.ISAproj.model.Lek;
-import com.team_08.ISAproj.model.Pacijent;
+import com.team_08.ISAproj.exceptions.CookieNotValidException;
+import com.team_08.ISAproj.model.*;
 import com.team_08.ISAproj.model.enums.OblikLeka;
 import com.team_08.ISAproj.model.enums.TipLeka;
-import com.team_08.ISAproj.repository.LekRepository;
-import com.team_08.ISAproj.repository.PacijentRepository;
-import com.team_08.ISAproj.service.LekService;
-import com.team_08.ISAproj.service.PacijentService;
+import com.team_08.ISAproj.repository.*;
+import com.team_08.ISAproj.service.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -23,8 +21,12 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.transaction.Transactional;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import static com.team_08.ISAproj.constants.PacijentConstants.*;
 
@@ -37,20 +39,47 @@ import static org.mockito.Mockito.*;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class serviceTests {
-    
-	@Mock
-	private PacijentRepository pacijentRepositoryMock;
+	//klase
 	@Mock
 	private Pacijent pacijentMock;
 	@Mock
-	private LekRepository lekRepositoryMock;
+	private ApotekaLek apotekaLekMock;
+	@Mock
+	private Rezervacija rezervacijaMock;
+	@Mock
+	private Set<Odsustvo> odsustvaMock;
+	@Mock
+	private List<Pregled> preglediMock;
 
-	
+	//repos
+	@Mock
+	private PregledRepository pregledRepositoryMock;
+	@Mock
+	private OdsustvoRepository odsustvoRepositoryMock;
+	@Mock
+	private PacijentRepository pacijentRepositoryMock;
+	@Mock
+	private LekRepository lekRepositoryMock;
+    @Mock
+	private ApotekaLekRepository apotekaLekRepositoryMock;
+    @Mock
+	private RezervacijaRepository rezervacijaRepositoryMock;
+
+    //servisi
+	@InjectMocks
+	private PregledService pregledService;
+	@InjectMocks
+	private OdsustvoService odsustvoService;
 	@InjectMocks
 	private PacijentService pacijentService;
 	@InjectMocks
 	private LekService lekService;
-	
+	@InjectMocks
+	private ApotekaLekService apotekaLekService;
+	@InjectMocks
+	private RezervacijaService rezervacijaService;
+
+	// ajducki testovi
 	@Test 
 	public void testFindOnePacijentById() {
 		when(pacijentRepositoryMock.findOneById(DB_PACIJENT_ID)).thenReturn(pacijentMock);
@@ -79,7 +108,7 @@ public class serviceTests {
 		verify(lekRepositoryMock, times(1)).findAllLekoviByApotedaIdPage(DB_APOTEKA_ID, "brufen", pageRequest);
         verifyNoMoreInteractions(lekRepositoryMock);
 	}
-	
+
 	@Test(expected = DataIntegrityViolationException.class)
 	@Transactional
 	@Rollback(true)
@@ -101,5 +130,75 @@ public class serviceTests {
 		// verifikacija interakcije sa mock objektima  
 		verify(lekRepositoryMock, times(1)).save(lek);
         verifyNoMoreInteractions(lekRepositoryMock);
+	}
+	//nikolini testovi
+	@Test
+	public void testFindAllByApotekaId() {
+		//apotekaLekService.findOneByApoteka(DB_APOTEKA_ID);
+		when(apotekaLekRepositoryMock.findApotekaLekById(1L, DB_APOTEKA_ID)).thenReturn(apotekaLekMock);
+
+		ApotekaLek dbApotekaLek = apotekaLekService.findInApotekaLek(1L, DB_APOTEKA_ID);
+
+		assertEquals(apotekaLekMock, dbApotekaLek);
+		verify(apotekaLekRepositoryMock, times(1)).findApotekaLekById(1L, DB_APOTEKA_ID);
+		verifyNoMoreInteractions(apotekaLekRepositoryMock);
+	}
+
+	//brcini testovi
+	@Test
+	@Transactional
+	public void testFetchRezervacije(){
+		LocalDateTime tommorow = LocalDateTime.now().plusDays(1);
+
+		when(rezervacijaRepositoryMock.fetchRezervacijaWithLekoviByIdAndApotekaIdBeforeRok(
+				1L, 1L, tommorow)).thenReturn(rezervacijaMock);
+
+		Rezervacija rezervacija = rezervacijaService.fetchRezervacijaWithLekoviByIdAndApotekaIdBeforeRok(
+				1L, 1L, tommorow);
+
+		assertEquals(rezervacija, rezervacijaMock);
+
+		verify(rezervacijaRepositoryMock, times(1)).fetchRezervacijaWithLekoviByIdAndApotekaIdBeforeRok(
+				1L, 1L, tommorow);
+		verifyNoMoreInteractions(rezervacijaRepositoryMock);
+	}
+
+	@Test
+	public void testFetchOdsustva(){
+		LocalDateTime start = LocalDateTime.now();
+		LocalDateTime end = LocalDateTime.now().plusDays(7);
+
+		when(odsustvoRepositoryMock.fetchOdsustvaByZdravstveniRadnikCookieInDateRange("dusan-dusan", start, end))
+				.thenReturn(odsustvaMock);
+
+		Set<Odsustvo> odsustva = odsustvoService.fetchOdsustvaByZdravstveniRadnikCookieInDateRange("dusan-dusan", start, end);
+
+		assertEquals(odsustva, odsustvaMock);
+
+		verify(odsustvoRepositoryMock, times(1)).fetchOdsustvaByZdravstveniRadnikCookieInDateRange(
+				"dusan-dusan", start, end);
+		verifyNoMoreInteractions(odsustvoRepositoryMock);
+	}
+
+	@Test
+	public void testFetchPregledi(){
+		LocalDateTime start = LocalDateTime.now().minusDays(30);
+		LocalDateTime end = LocalDateTime.now().plusDays(7);
+
+		when(pregledRepositoryMock.fetchAllWithPreporuceniLekoviInDateRangeByZdravstveniRadnik("dusan-dusan", start, end))
+				.thenReturn(preglediMock);
+
+		List<Pregled> pregledi=null;
+		try {
+			pregledi = pregledService.fetchAllWithPreporuceniLekoviInDateRangeByZdravstveniRadnik("dusan-dusan", start, end);
+		} catch (CookieNotValidException e) {
+			e.printStackTrace();
+		}
+
+		assertEquals(pregledi, preglediMock);
+
+		verify(pregledRepositoryMock, times(1)).fetchAllWithPreporuceniLekoviInDateRangeByZdravstveniRadnik(
+				"dusan-dusan", start, end);
+		verifyNoMoreInteractions(pregledRepositoryMock);
 	}
 }
